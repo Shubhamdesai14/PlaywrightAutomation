f8c760d4dd46049ce95ae2548ad7ae842b68f076
Shubham Desai <shubham.shankar@zitcha.com>
2025-06-27 19:48:01 +0530

    handling grouping of tests

---
+++ tests/Grouping.spec.js
@@ -0,0 +1,50 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeAll(async()=>{
+        console.log('this is beforeAll hook....')
+})
+
+
+test.afterAll(async()=>{
+        console.log('this is afterAll hook....')
+})
+
+
+test.beforeEach(async()=>{
+        console.log('this is beforeEach hook....')
+})
+
+test.afterEach(async()=>{
+        console.log('this is afterEach hook....')
+})
+
+
+test.describe('Group1', ()=>{
+
+test('Test1', async({page})=>{
+    console.log('this is the test1....')
+})
+
+
+test('Test2', async({page})=>{
+    console.log('this is the test2....')
+})
+
+})
+
+
+
+
+test.describe('Group2', ()=>{
+
+test('Test3', async({page})=>{
+    console.log('this is the test3....')
+})
+
+
+test('Test4', async({page})=>{
+    console.log('this is the test4....')
+})
+
+
+})
